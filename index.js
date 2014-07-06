b1775eda80be22aefbf59ce1644ef1ed0835f4d1
Jarno Rantanen <jarno@jrw.fi>
2014-07-06 19:04:52 +0300

    Moved the code over to the dedicated JS-file.

--- index.js
+++ index.js
@@ -2,20 +2,225 @@
     var todo = Object.keys(modules);
     (function next(api) {
         var module = todo.shift();
-        if (module) {
-            modules[module](api, function(declarations) {
+        if (!module) { return } // all done!
+        modules[module](api, function(declarations) {
+            if (declarations) {
                 api[module] = {};
                 Object.keys(declarations).forEach(function(key) {
                     api[module][key] = declarations[key];
                 });
-                next(api);
-            });
-        }
+            }
+            next(api);
+        });
     })({});
 })({
 
     dom: function(api, declare) {
-        // TODO
+
+        declare({
+            document: document,
+            body: document.body,
+            header: document.querySelector('header'),
+            main: document.querySelector('main'),
+            aside: document.querySelector('aside'),
+            map: document.getElementById('map-container')
+        });
+
+    },
+
+    data: function(api, declare) {
+
+        function getJSON(url, callback) {
+            var request = new XMLHttpRequest();
+            request.open('GET', url, true);
+            request.onload = function() {
+                if (request.status >= 200 && request.status < 400){
+                    callback(JSON.parse(request.responseText));
+                } else {
+                    throw new Error('Server returned an error');
+                }
+            };
+            request.onerror = function() {
+                throw new Error('Connection error');
+            };
+            request.send();
+        }
+
+        function getParallel(map, callback) {
+            var done = {};
+            Object.keys(map).forEach(function(key) {
+                getJSON(map[key], function(data) {
+                    done[key] = data;
+                    if (Object.keys(done).length === Object.keys(map).length) {
+                        callback(done);
+                    }
+                });
+            });
+        }
+
+        getParallel({
+            media: 'media.json',
+            tracks: 'tracks.json'
+        }, function(data) {
+            data.getByGroupID = function(array, groupID) {
+                var match = array.filter(function(group) {
+                    return group.groupID === groupID;
+                });
+                return match.length === 1 ? match[0] : null;
+            };
+            declare(data);
+        });
+
+    },
+
+    map: function(api, declare) {
+
+        var TRAVEL_TYPE_COLORS = {
+            BUS: 'blue',
+            WALK: 'red',
+            unknown: 'gray'
+        };
+
+        var map = new google.maps.Map(api.dom.map, {
+            center: new google.maps.LatLng(0, 0), // TODO: Are more sensible default..?
+            zoom: 8
+        });
+
+        var marker;
+
+        // @example coordinates = "37.551,126.988"
+        function updateMarker(coordinates) {
+            if (marker) {
+                marker.setMap(null); // remove previous marker
+            }
+            if (!coordinates) {
+                return;
+            }
+            coordinates = {
+                lat: parseFloat(coordinates.split(',')[0]),
+                lng: parseFloat(coordinates.split(',')[1])
+            };
+            marker = new google.maps.Marker({
+                position: coordinates, // or: new google.maps.LatLng(0, 0),
+                map: map
+            });
+            // map.panTo(coordinates);
+        }
+
+        var polylines;
+        var currentlyDrawnGroupID;
+
+        function updatePolyline(groupID) {
+            if (groupID === currentlyDrawnGroupID) { return }
+            currentlyDrawnGroupID = groupID;
+            var group = api.data.getByGroupID(api.data.tracks, groupID);
+            if (!group) { return }
+            if (polylines) {
+                polylines.forEach(function(polyline) {
+                    polyline.setMap(null); // remove from map
+                });
+            }
+            var bounds = new google.maps.LatLngBounds();
+            polylines = group.segments.map(function(segment) {
+                var coords = segment.points.map(function(pointTuple) {
+                    return new google.maps.LatLng(pointTuple[0], pointTuple[1]);
+                });
+                var polyLine = new google.maps.Polyline({
+                    path: coords,
+                    geodesic: true,
+                    strokeColor: TRAVEL_TYPE_COLORS[segment.type] || TRAVEL_TYPE_COLORS.unknown,
+                    strokeOpacity: 1.0,
+                    strokeWeight: 2
+                });
+                polyLine.setMap(map);
+                coords.forEach(function(c) {
+                    bounds.extend(c);
+                });
+                return polyLine;
+            });
+            map.fitBounds(bounds);
+        }
+
+        var currentlyFocused;
+
+        declare({
+            focusMediaItem: function(mediaEl) {
+                if (!mediaEl || currentlyFocused === mediaEl) { return }
+                updateMarker(mediaEl.dataset.location);
+                updatePolyline(mediaEl.dataset.groupID);
+            }
+        });
+
+    },
+
+    aside: function(api, declare) {
+
+        var asideToggleAt = api.dom.header.offsetHeight + api.dom.header.offsetTop;
+        var aside = api.dom.aside;
+
+        function onScroll() {
+            var scrollTop = api.dom.body.scrollTop;
+            if (scrollTop > asideToggleAt && !aside.className) {
+                aside.className = 'active';
+            } else if (scrollTop <= asideToggleAt && aside.className === 'active') {
+                aside.className = '';
+            }
+        }
+
+        api.dom.document.addEventListener('scroll', onScroll);
+        onScroll();
+
+        var currentlyFocused;
+
+        declare({
+            focusMediaItem: function(mediaEl) {
+                if (!mediaEl || currentlyFocused === mediaEl) { return }
+                currentlyFocused = mediaEl;
+                var group = api.data.getByGroupID(api.data.media, mediaEl.dataset.groupID);
+                if (!group) { return }
+                api.dom.aside.querySelector('h2').innerText = group.title;
+                api.map.focusMediaItem(mediaEl);
+            }
+        });
+
+    },
+
+    media: function(api, declare) {
+
+        api.data.media.forEach(function(group) {
+            group.media.forEach(function(media) {
+                var el = document.createElement('img');
+                el.setAttribute('src', media.url);
+                el.dataset.groupID = group.groupID;
+                el.dataset.location = media.location;
+                el.dataset.timestamp = media.timestamp;
+                api.dom.main.appendChild(el);
+            });
+        });
+
+        var asideWidth = api.dom.aside.offsetWidth;
+
+        function deactivateCurrent() {
+            (api.dom.main.querySelector('img.active') || {}).className = '';
+        }
+
+        function onScroll() {
+            var newTarget = api.dom.document.elementFromPoint(asideWidth + (window.innerWidth - asideWidth) / 2, window.innerHeight / 2);
+            if (newTarget === api.dom.header) {
+                deactivateCurrent();
+                api.aside.focusMediaItem();
+            } else if (newTarget.className !== 'active') {
+                deactivateCurrent();
+                newTarget.className = 'active';
+                api.aside.focusMediaItem(newTarget);
+            }
+        }
+
+        api.dom.document.addEventListener('scroll', onScroll);
+        onScroll();
+
+        declare();
+
     }
 
 });
